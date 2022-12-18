a50130b5a78aaefa47244973d14372dfe21b4127
Andrz Zack <guillermo07_chavez07@outlook.es>
2022-12-18 15:41:33 -0500

    Realizando un juego

---
+++ Mockepon/index.js
@@ -0,0 +1,85 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+
+app.use(cors())
+app.use(express.json())
+
+const jugadores = []
+
+class Jugador {
+    constructor(id) {
+        this.id = id
+    }
+
+    asignarMokepon(mokepon){
+        this.mokepon = mokepon
+    }
+    actualizarPosicion(x, y){
+        this.x = x
+        this.y = y
+    }
+}
+
+class Mokepon{constructor(nombre){this.nombre=nombre}}
+
+
+
+
+ app.get("/unirse", (req, res) => {
+
+    const id = `${Math.random()}`
+
+    const jugador = new Jugador(id)
+
+    jugadores.push(jugador)
+
+    res.setHeader("Access-Control-Allow-Origin", "*")
+
+
+    res.send(id)
+})
+
+app.post("/mokepon/:jugadorId", (req, res) => {
+    const jugadorId = req.params.jugadorId || ""
+    const nombre = req.body.mokepon || ""
+    const mokepon = new Mokepon(nombre)
+    
+    const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+
+    if (jugadorIndex >= 0) {
+        jugadores[jugadorIndex].asignarMokepon(mokepon)
+    }
+     console.log(jugadores)
+    console.log(jugadorId) 
+     res.end()
+}) 
+//empieza clase 78
+app.post("/mokepon/:jugadorId/posicion", (req, res) => {
+    const jugadorId = req.params.jugadorId || ""
+    const x = req.body.x || 0
+    const y = req.body.y || 0
+
+
+    const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+
+    if (jugadorIndex >= 0) {
+        jugadores[jugadorIndex].actualizarPosicion(x, y)
+    }
+    //empieza clase 79 
+    const enemigos = jugadores.filter((jugador) => jugadorId !== jugador.id)
+
+
+
+    res.end({
+        enemigos
+    })
+})
+
+
+ app.listen(8080, () => {
+    console.log("servidor funcionando!")
+})  
+
